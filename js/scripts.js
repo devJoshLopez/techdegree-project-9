bc5927b69b27f9434b00d3f84505a8bcebf99c74
Josh Lopez <devjoshlopez@gmail.com>
2019-01-28 18:47:40 -0700

    scrollreveal

--- js/scripts.js
+++ js/scripts.js
@@ -8,38 +8,33 @@ $(document).ready(function () {
   });
 
   // add glitch effect on img on hover and remove when not
-  $('.portfolio-item').hover(
+  $('.card-item').hover(
     function () {
-      $(this).find('.portfolio-item-image').addClass("glitch-img");
+      $(this).find('.card-item-image').addClass("glitch-img");
 
-      $(this).find('.portfolio-item-image').mgGlitch({
-        // set 'true' to stop the plugin
+      $(this).find('.card-item-image').mgGlitch({
         destroy: false,
-        // set 'false' to stop glitching
         glitch: true,
-        // set 'false' to stop scaling
         scale: true,
-        // set 'false' to stop glitch blending
         blend: true,
-        // select blend mode type
         blendModeType: 'hue',
-        // set min time for glitch 1 elem
         glitch1TimeMin: 200,
-        // set max time for glitch 1 elem
         glitch1TimeMax: 400,
-        // set min time for glitch 2 elem
         glitch2TimeMin: 10,
-        // set max time for glitch 2 elem
         glitch2TimeMax: 100,
       });
     },
     function () {
-      $(this).find('.portfolio-item-image').removeClass("glitch-img");
-      $(this).find('.portfolio-item-image').mgGlitch({
-        // set 'true' to stop the plugin
+      $(this).find('.card-item-image').removeClass("glitch-img");
+      $(this).find('.card-item-image').mgGlitch({
         destroy: true
       });
     }
   );
+
+  // show cards when scrolling
+  ScrollReveal({
+    reset: true
+  }).reveal('.card-item');
 
 });
